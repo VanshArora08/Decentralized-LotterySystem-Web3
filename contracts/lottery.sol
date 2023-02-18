a18eaacee99f083e75ab6c59a65ddfb78966e6bc
VanshArora08 <kumar.vansharora@gmail.com>
2023-02-18 18:50:21 +0530

    added getters and vrf coordinator

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,13 +1,22 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Lottery {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract Lottery is VRFConsumerBaseV2 {
     //state variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    event Lottery_Enter(address indexed player);
 
-    constructor(uint256 entranceFees) {
+    constructor(
+        uint256 entranceFees,
+        address vrfCoordinator
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFees;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
     }
 
     error lottery__NotEnoughETHEntered();
@@ -17,5 +26,23 @@ contract Lottery {
             revert lottery__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit Lottery_Enter(msg.sender);
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal virtual override {}
+
+    function requestRandomWinner() external {
+        //
+    }
+
+    function getPlayerAt(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getEntryFees() public view returns (uint256) {
+        return i_entranceFee;
     }
 }
